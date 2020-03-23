ed94f961b5d6665e4af1bcf17a0bbaf6a4af18ec
Sumit Lal <sumitlal.techdev@gmail.com>
2020-03-23 16:56:10 -0700

    Finished stringifyJSON function

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,35 @@
 
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function(obj) {
+var stringifyJSON = function (obj) {
   // your code goes here
-};
+
+  if (obj === null) {
+    return 'null';
+  } else if (obj === undefined) {
+    return "undefined";
+  } else if (typeof obj === 'string') {
+    return `"${obj}"`;
+  } else if (Array.isArray(obj)) {
+    let str = '';
+
+    for (let i = 0; i < obj.length; i++) {
+      str = str.concat(stringifyJSON(obj[i]) + ',');
+    }
+
+    return `[${str.slice(0, str.length - 1)}]`;
+  } else if (typeof obj === 'object') {
+    let str = '';
+
+    for (let key in obj) {
+      if (obj[key] !== undefined && typeof obj[key] !== 'function') {
+        str = str.concat(stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + ',')
+      }
+    }
+
+    return `{${str.slice(0, str.length - 1)}}`;
+
+  } else {
+    return obj.toString();
+  }
+}
